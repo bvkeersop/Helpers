043e88f5b60d23f099779363fac17bd5e4406175
Bart van Keersop <bart.van.keersop@gmail.com>
2022-05-22 14:21:33 +0200

    Added http fakes and builders

---
+++ src/Helpers.TestHelpers.Test.Unit/Fakes/FakeHttpMessageHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using TestHelpers.Fakes;
+
+namespace Helpers.TestHelpers.Test.Unit.Fakes
+{
+    [TestClass]
+    public class FakeHttpMessageHandlerTests
+    {
+        [TestMethod]
+        public void Ctor_ResponsesIsEmpty_ThrowsArgumentException()
+        {
+            // Arrange
+            var emptyResponseDict = new Dictionary<string, HttpResponseMessage>();
+
+            // Act
+            var action = () => new FakeHttpMessageHandler(emptyResponseDict);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
